a66f433c93910755436e376c5807e452e22b4beb
Dávid Hrubos <hrubos.david@hotmail.com>
2024-07-16 08:14:03 +0200

    Install nugets,create structure,repository

---
+++ URLShortener/Repositories/UrlRepository.cs
@@ -0,0 +1,24 @@
+﻿using Dapper;
+using Microsoft.Data.SqlClient;
+using System.Data;
+
+namespace URLShortener.Repositories
+{
+    public class UrlRepository
+    {
+        private readonly string _connectionString;
+        public UrlRepository(string connectionString)
+        {
+            _connectionString = connectionString;
+        }
+
+        public async Task AddUrlAsync(string longURL,string shortURL)
+        {
+            using(IDbConnection db= new SqlConnection(_connectionString))
+            {
+                var query = "INSERT INTO Urls (LongUrl, ShortUrl) VALUES (@LongUrl,@ShortUrl )";
+                await db.ExecuteAsync(query,new {LongUrl=longURL,ShortURL = shortURL});
+            }
+        }
+    }
+}
